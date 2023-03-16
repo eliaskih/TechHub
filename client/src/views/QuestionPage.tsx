759d677489dbbef72c76b4850f7359e77804c89c
elias kihlstenius <elias.kihlstenius@gmail.com>
2023-03-16 13:14:40 +0100

    added content to question page

--- client/src/views/QuestionPage.tsx
+++ client/src/views/QuestionPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import { Question } from "../Questions";
 import { useParams } from "react-router-dom";
+import moment from "moment";
 
 export default function QuestionPage() {
   const { id } = useParams();
@@ -18,5 +19,29 @@ export default function QuestionPage() {
   });
   console.log(question);
 
-  return <div>QuestionPage</div>;
+  if (isLoading)
+    return (
+      <div className="flex justify-center py-10">
+        <div className="w-7 h-7 rounded-full border-4 border-t-orange-500 animate-spin border-gray-700"></div>
+      </div>
+    );
+
+  if (isError) return <div>oops, something went wrong...</div>;
+
+  console.log(question);
+
+  return (
+    <div className="max-w-4xl mx-auto py-10">
+      <div className="border-b pb-4 mb-4">
+        <h2 className="text-2xl mb-2 text-gray-700">{question.title}</h2>
+        <p className="text-sm text-gray-800">
+          Asked:{" "}
+          <span className="text-gray-800">
+            {moment(question.createdAt).startOf("day").fromNow()}
+          </span>
+        </p>
+      </div>
+      <p className="">{question.description}</p>
+    </div>
+  );
 }
